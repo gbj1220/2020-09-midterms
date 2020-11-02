57adda0e0b1901a6cd5863a893400933d4ee0cbf
Gregory.Johnson <3531op@gmail.com>
2020-11-02 13:27:50 -0500

    Initial commit/Completed assignment

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
 // Your code below.
+const isValidPassword = function (str) {
+  return str.length < 12 || str.includes('.') || str.includes(' ') ? false : true
+}
+
+const onlyCs = function (percentage) {
+  // const letterGrade = 
+  return percentage.filter((percent) => percent >= 70 && percent <= 79 ? true : false)
+}
+
+const countBs = function (percentage) {
+  newArr = [];
+  percentage.filter((percent) => percent >= 80 && percent <= 89 ? newArr.push(percent) : false)
+  let counter = 0;
+  for (percent of percentage) {
+    if (percent >= 80 && percent <= 89) {
+      counter++
+    }
+  } return counter
+}
+const lastIndexOfPunctuation = function (str) {
+  const punc = ['?!.']
+  
+  
+  for (values of str) {}
+  if (str.includes('?')) {
+    return str.lastIndexOf('?')
+  
+  }else if (str.includes('!')) {
+    return str.lastIndexOf('!')
+  
+  }else if (str.includes('.')) {
+    return str.lastIndexOf('.')
+  
+  }else if (str.includes(punc)) {
+    return str.lastIndexOf(punc)
+  
+  }else{
+    return str.lastIndexOf()
+  }
+}
+
 
 
 
